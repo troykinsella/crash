4dbb0953a28cd6035155d5e2e383358daa0bdd01
Troy Kinsella <troy.kinsella@startmail.com>
2016-10-11 12:51:13 -0700

    Fix docs crash executions

--- .build.sh
+++ .build.sh
@@ -4,7 +4,7 @@ set -xe
 
 compile() {
   go build -o crash -v github.com/troykinsella/crash/cmd
-  ln -s crash $GOPATH/bin/crash
+  ln -s $PWD/crash $GOPATH/bin/crash
 }
 
 cross_compile() {
@@ -12,7 +12,7 @@ cross_compile() {
     -os="darwin linux windows" \
     -output="crash_{{.OS}}_{{.Arch}}" \
     github.com/troykinsella/crash/cmd
-  ln -s crash_linux_amd64 $GOPATH/bin/crash
+  ln -s $PWD/crash_linux_amd64 $GOPATH/bin/crash
 }
 
 # Is a tag build?
