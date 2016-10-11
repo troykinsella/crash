d40c09bb2099e514472ba12440ec620b93f32376
Troy Kinsella <troy.kinsella@startmail.com>
2016-10-11 10:59:36 -0700

    Fix docs crash executions

--- .build.sh
+++ .build.sh
@@ -2,16 +2,22 @@
 
 set -xe
 
-make_binaries() {
-    gox -arch="amd64" \
-        -os="darwin linux windows" \
-        -output="crash_{{.OS}}_{{.Arch}}" \
-        github.com/troykinsella/crash/cmd
-    ln -s crash_linux_amd64 $GOPATH/bin/crash
+compile() {
+  go build -o crash -v github.com/troykinsella/crash/cmd
+}
+
+cross_compile() {
+  gox -arch="amd64" \
+    -os="darwin linux windows" \
+    -output="crash_{{.OS}}_{{.Arch}}" \
+    github.com/troykinsella/crash/cmd
 }
 
 # Is a tag build?
 if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ -n "$TRAVIS_TAG" ]; then
-    make_binaries
-    sha256sum crash_* > sha256sum.txt
+  cross_compile
+  ln -s crash_linux_amd64 $GOPATH/bin/crash
+  sha256sum crash_* > sha256sum.txt
+else
+  compile
 fi
